49c2e768c23b58e91298c2c2ba40cc8c7545ddf5
Lê Hồng Việt <vietlh@ttc-solutions.com.vn>
2021-04-14 10:17:08 +0000

    Feature/select advance

---
+++ src/app/shared/controls/abstract-control.directive.ts
@@ -0,0 +1,36 @@
+import { ControlValueAccessor } from '@angular/forms';
+import { Input, Directive } from '@angular/core';
+import { InputBoolean } from 'ng-zorro-antd/core/util';
+
+@Directive()
+export abstract class AbstractControlDirective<T = any> implements ControlValueAccessor {
+
+    controlValue: T;
+
+    @Input() @InputBoolean() disabled = false;
+    @Input() @InputBoolean() autoFocus = false;
+    @Input() placeholder = '';
+
+    onChangeFn: (val: T) => void = () => void 0;
+    onTouchedFn: () => void = () => void 0;
+
+    writeValue(obj: any): void {
+        this.controlValue = obj;
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChangeFn = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouchedFn = fn;
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    onModelChange(value: T) {
+        this.onChangeFn(value);
+    }
+}
---
+++ src/app/shared/controls/select-advance/select-advance.component.ts
@@ -0,0 +1,89 @@
+import { Component, Input, forwardRef, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AbstractControlDirective } from '../abstract-control.directive';
+import { debounceTime, distinctUntilChanged, finalize, map, startWith, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { DestroyService } from '@shared/services/destroy.service';
+
+export interface ParamsSelectAdvance {
+  page?: number;
+  q?: string;
+}
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'select-advance',
+  templateUrl: './select-advance.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectAdvanceComponent),
+      multi: true
+    },
+    DestroyService
+  ],
+})
+export class SelectAdvanceComponent extends AbstractControlDirective implements OnInit {
+
+  isLoading = false;
+  page = 0;
+  timeout: any;
+  isDone = false;
+  options = [];
+  search$: Subject<string>;
+  loadMore$: Subject<any>;
+  q: string;
+  @Input() getOptionsFn: (params: ParamsSelectAdvance) => Observable<any>;
+  @Output() readonly csClear = new EventEmitter<void>();
+
+  constructor(
+    private destroy: DestroyService
+  ) {
+    super();
+    this.search$ = new Subject<string>();
+    this.loadMore$ = new Subject<any>();
+  }
+
+  ngOnInit() {
+    this.loadMore();
+    this.onSearch();
+  }
+
+  async writeValue(obj: any) {
+    super.writeValue(obj);
+  }
+
+  onSearch() {
+    this.search$.pipe(
+      startWith(''),
+      distinctUntilChanged(),
+      debounceTime(500),
+      tap(q => this.q = q),
+      switchMap(q => this.getOptionsFn({ page: 1, q })),
+      finalize(() => {
+        this.isLoading = false;
+      }),
+      takeUntil(this.destroy)
+    ).subscribe(data => {
+      this.options = data;
+    });
+  }
+
+  async loadMore(): Promise<void> {
+    this.loadMore$.pipe(
+      tap(() => this.isLoading = true),
+      switchMap(x => this.getOptionsFn({ page: this.page++, q: this.q })),
+      finalize(() => this.isLoading = false),
+      takeUntil(this.destroy)
+    ).subscribe(this.pushToOption);
+  }
+
+  private pushToOption(data: any[]) {
+    this.options = this.options.concat(data.filter(x => !this.options.some(y => y.value === x.value)));
+  }
+
+  onClear() {
+    this.csClear.emit();
+  }
+
+}
