5d132320031aac76fbc1f36b69e67eba86cce1b6
Nguyễn Hữu Giang <giangnh@ttc-solutions.com.vn>
2021-04-19 07:17:55 +0000

    Merge branch 'fix/router' into 'dev'
    Fix/router
    
    See merge request beauty-up/cms-frontend!25
    
    (cherry picked from commit 23a6079fecb191d8f0542fcf17e392310bf7d2bf)
    
    e4ba6644 fix router

--- src/app/authentication/authentication.routing.ts
+++ src/app/authentication/authentication.routing.ts
@@ -2,6 +2,8 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { Error404Component } from './error404/error404.component';
+import { Error500Component } from './error500/error500.component';
 
 const routes: Routes = [
   {
@@ -28,7 +30,26 @@ const routes: Routes = [
     data: {
       title: 'Forgot password?'
     }
-  }
+  },
+  {
+    path: 'error',
+    children: [
+      {
+        path: '404',
+        component: Error404Component,
+        data: {
+          title: 'Error 404'
+        }
+      },
+      {
+        path: '500',
+        component: Error500Component,
+        data: {
+          title: 'Error 500'
+        }
+      }
+    ]
+  },
 ];
 
 export const AuthenticationRoutes = RouterModule.forChild(routes);
