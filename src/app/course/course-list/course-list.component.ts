35fd13405e5e50b2cff742bb957fef12622247fb
DESKTOP-76QVNDH\admin <giangnh@ttc-solutions.com.vn>
2021-04-22 17:32:07 +0700

    course-list

--- src/app/course/course-list/course-list.component.ts
+++ src/app/course/course-list/course-list.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TeacherApiService } from '@shared/api/teacher.api.service';
 import { DataTableContainer } from '@shared/class/data-table-container';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Course } from 'types/models/course';
-import { QueryResult } from 'types/typemodel';
+import { DataTableColumnMetaData, QueryResult } from 'types/typemodel';
+import { Option } from '@shared/interfaces/option.type';
+import { CourseTypesApiService } from '@shared/api/course-types.api.service';
+import { CourseApiService } from '@shared/api/course.api.service';
 
 @Component({
   selector: 'app-course-list',
@@ -13,11 +18,51 @@ import { QueryResult } from 'types/typemodel';
 })
 export class CourseListComponent extends DataTableContainer<Course> implements OnInit {
   search: FormGroup;
+  metaData: DataTableColumnMetaData[] = [
+    {
+      key: 'name',
+      name: 'Khóa học',
+      sortable: false,
+    },
+    {
+      key: 'teacher',
+      name: 'Giảng viên',
+      sortable: false,
+    },
+    {
+      key: 'courseType',
+      name: 'Loại khóa học',
+      sortable: false,
+    },
+    {
+      key: 'studentPrice',
+      name: 'Giá học viên',
+      sortable: true,
+    },
+    {
+      key: 'teacherPrice',
+      name: 'Giá đối tác',
+      sortable: true,
+    },
+    {
+      key: 'numberOfLearner',
+      name: 'Tổng học viên',
+      sortable: true,
+    },
+    {
+      key: 'created',
+      name: 'Ngày tạo',
+      sortable: true,
+    },
+  ];
 
   constructor(
     route: ActivatedRoute,
     router: Router,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private teacherApi: TeacherApiService,
+    private courseTypeApi: CourseTypesApiService,
+    private courseApi: CourseApiService
   ) {
     super(route, router);
   }
@@ -26,7 +71,24 @@ export class CourseListComponent extends DataTableContainer<Course> implements O
   }
 
   protected fetch(): Observable<QueryResult<Course>> {
-    return of(null);
+    const params = {
+      limit: this.quantity,
+      page: this.page,
+    };
+    const { specializationId, q, status } = this.params;
+    return this.courseApi.getList({ ...params, specializationId, q, status });
+  }
+
+  teacher$ = (params): Observable<Option[]> => {
+    return this.teacherApi.getList(params).pipe(map(res => res.items.map(x => {
+      return { value: x.id, label: x.fullName };
+    })));
+  }
+
+  type$ = (params): Observable<Option[]> => {
+    return this.courseTypeApi.getList(params).pipe(map(res => res.items.map(x => {
+      return { value: x.id, label: x.name };
+    })));
   }
 
   buildForm() {
--- types/models/course.ts
+++ types/models/course.ts
@@ -1,7 +1,13 @@
-import { Entity } from "types/typemodel";
+import { Entity } from 'types/typemodel';
 
 export interface Course extends Entity {
-
+  name: string;
+  teacherAvatar: string;
+  teacherName: string;
+  typeName: string;
+  studentPrice: number;
+  partnerPrice: number;
+  totalStudent: number;
 }
 export interface Specialization extends Entity {
   name: string;
