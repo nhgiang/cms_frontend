89d2b64f0693c79874ae82c89d579af1af5ebac9
vietlh <viet150597@gmail.com>
2021-04-27 14:21:20 +0700

    ok

--- src/app/shared/controls/multiple-selection/multiple-selection.component.ts
+++ src/app/shared/controls/multiple-selection/multiple-selection.component.ts
@@ -1,10 +1,7 @@
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { DestroyService } from '@shared/services/destroy.service';
-import { Observable, Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged, finalize, startWith, switchMap, takeUntil, tap } from 'rxjs/operators';
-import { AbstractControlDirective } from '../abstract-control.directive';
-import { ParamsSelectAdvance } from '../select-advance/select-advance.component';
+import { SelectAdvanceComponent } from '../select-advance/select-advance.component';
 
 @Component({
   selector: 'app-multiple-selection',
@@ -19,40 +16,5 @@ import { ParamsSelectAdvance } from '../select-advance/select-advance.component'
     DestroyService
   ],
 })
-export class MultipleSelectionComponent extends AbstractControlDirective implements OnInit {
-
-  @Input() apiFn: (params: ParamsSelectAdvance) => Observable<any>;
-  @Input() listOfSelectedValue: string[];
-  listOfOption: string[] = [];
-  page = 1;
-  q: string;
-  search$: Subject<string>;
-  isLoading = false;
-
-  constructor(
-    private destroy: DestroyService
-  ) {
-    super();
-    this.search$ = new Subject<string>();
-  }
-
-  ngOnInit(): void {
-    this.search$.pipe(
-      startWith(''),
-      debounceTime(500),
-      distinctUntilChanged(),
-      tap(q => {
-        this.q = q;
-        this.page = 1;
-        this.isLoading = true;
-      }),
-      switchMap(() => this.apiFn({ page: this.page, q: this.q }).pipe(finalize(() => this.isLoading = false))),
-      takeUntil(this.destroy)
-    ).subscribe(data => {
-      this.listOfOption = data;
-    });
-  }
-
-
-
+export class MultipleSelectionComponent extends SelectAdvanceComponent {
 }
