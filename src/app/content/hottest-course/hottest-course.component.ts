47c4100a916a10dd43da7e1e92bc6796540e17da
giangnh <giangnh@ttc-solutions.com.vn>
2021-05-05 08:03:17 +0700

    hottest-course

--- src/app/content/hottest-course/hottest-course.component.ts
+++ src/app/content/hottest-course/hottest-course.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
 import { CourseApiService } from '@shared/api/course.api.service';
+import { IPaginate } from '@shared/interfaces/paginate.type';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hottest-course',
@@ -8,16 +11,27 @@ import { CourseApiService } from '@shared/api/course.api.service';
 })
 export class HottestCourseComponent implements OnInit {
   dummy: number[];
+  form: FormArray;
 
   constructor(
-    private courseApi: CourseApiService
+    private courseApi: CourseApiService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.dummy = Array(10).fill(0);
+    this.form = this.fb.array(this.dummy.map(() => this.fb.group({
+      courseId: [null]
+    })))
   }
 
-  // courses () {
-  //   return this.
-  // }
+  courses = (params: IPaginate) => {
+    return this.courseApi.getList(params).pipe(map(res => res.items.map(x => {
+      return { value: x.id, label: x.name };
+    })))
+  }
+
+  submit() {
+
+  }
 }
