ee0f47441aa1cca3a30557b70d6b3c9c553c122d
vietlh <viet150597@gmail.com>
2021-05-19 16:56:08 +0700

    fix token

--- src/app/shared/services/token.service.ts
+++ src/app/shared/services/token.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@angular/core';
 import jwt_decode from 'jwt-decode';
 import { Observable, timer } from 'rxjs';
 import * as moment from 'moment';
-import { switchMap } from 'rxjs/operators';
+import { finalize, switchMap, takeUntil } from 'rxjs/operators';
 import { environment } from '@env';
+import { Subject } from 'rxjs';
 
 export interface ITokenDecode {
   id: string;
@@ -18,20 +19,25 @@ export interface ITokenDecode {
 })
 export class TokenService {
 
+  public destroySubject: Subject<any>;
   private baseUrl = `${environment.api}/auth/refresh`;
 
   constructor(
     private httpClient: HttpClient
-  ) { }
+  ) {
+    this.destroySubject = new Subject();
+  }
 
   refreshTokenFn() {
     const refreshToken = localStorage.getItem('refreshToken');
-    const { exp } = jwt_decode(localStorage.getItem('token')) as ITokenDecode;
-    const timerReset = moment(exp * 1_000).subtract(new Date().getTime(), 'ms').unix() * 1_000 - 100_000;
-    timer(timerReset).pipe(switchMap(() => this.getNewToken({ refreshToken }))).subscribe(res => {
-      localStorage.setItem('token', res.accessToken);
-      this.refreshTokenFn();
-    });
+    if (refreshToken) {
+      const { exp } = jwt_decode(localStorage.getItem('token')) as ITokenDecode;
+      const timerReset = moment(exp * 1_000).subtract(new Date().getTime(), 'ms').unix() * 1_000 - 100_000;
+      timer(timerReset).pipe(switchMap(() => this.getNewToken({ refreshToken })), takeUntil(this.destroySubject)).subscribe(res => {
+        localStorage.setItem('token', res.accessToken);
+        this.refreshTokenFn();
+      });
+    }
   }
 
   private getNewToken(params: { refreshToken: string }): Observable<any> {
