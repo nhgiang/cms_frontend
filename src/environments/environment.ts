7eb2d5d050e7decf97ae8b8c92be649e33a38c72
Lê Ngọc Vũ <vuln@ttc-solutions.com.vn>
2021-06-18 08:25:50 +0000

    add loading to teacherview :wq

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // api: 'http://localhost:3000/v1'
-  api: 'https://qa.backend.beautyup.asia/v1'
+  api: 'https://qa.backend.beautyup.asia/v1',
 };
   
