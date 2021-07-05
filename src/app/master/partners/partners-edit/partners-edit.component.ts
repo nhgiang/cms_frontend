91453e56c18ed7417ab9db6d51cb23f0a2a4e74f
Duong <duongdt0291@gmail.com>
2021-07-05 08:22:04 +0700

    parterr: change field maxCourse to maxFileSizeUpload in form create/update partner

--- src/app/master/partners/partners-edit/partners-edit.component.ts
+++ src/app/master/partners/partners-edit/partners-edit.component.ts
@@ -1,8 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
 import { PartnersApiService } from '@shared/api/partners.api.service';
 import { TValidators } from '@shared/extentions/validators';
-import { replace } from 'lodash-es';
 import { NzModalRef } from 'ng-zorro-antd/modal';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { Observable, timer } from 'rxjs';
@@ -11,7 +15,7 @@ import { map, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-partners-edit',
   templateUrl: './partners-edit.component.html',
-  styleUrls: ['./partners-edit.component.scss']
+  styleUrls: ['./partners-edit.component.scss'],
 })
 export class PartnersEditComponent implements OnInit {
   @Input() data: any;
@@ -22,38 +26,63 @@ export class PartnersEditComponent implements OnInit {
     private partnersApiService: PartnersApiService,
     private fb: FormBuilder,
     private modalRef: NzModalRef<PartnersEditComponent>,
-    private notification: NzNotificationService,
-  ) { }
+    private notification: NzNotificationService
+  ) {}
 
   ngOnInit() {
     this.buildForm();
   }
 
   submit() {
-    this.form.get('domain').setValue(this.form.get('domain').value + '.beautyup.asia');
+    this.form
+      .get('domain')
+      .setValue(this.form.get('domain').value + '.beautyup.asia');
     const next = () => {
-      this.notification.success('Thành công', 'Cập nhật thông tin partner thành công!');
+      this.notification.success(
+        'Thành công',
+        'Cập nhật thông tin partner thành công!'
+      );
       this.modalRef.close(true);
     };
     const err = () => {
-      this.form.get('domain').setValue(this.form.get('domain').value.replace('.beautyup.asia', ''));
-      this.notification.error('Thất bại', 'Cập nhật thông tin partner thất bại!')
-    }
-    this.partnersApiService.update(this.data.id, this.form.value).subscribe(next, err);
+      this.form
+        .get('domain')
+        .setValue(this.form.get('domain').value.replace('.beautyup.asia', ''));
+      this.notification.error(
+        'Thất bại',
+        'Cập nhật thông tin partner thất bại!'
+      );
+    };
+    this.partnersApiService
+      .update(this.data.id, this.form.value)
+      .subscribe(next, err);
   }
 
   close() {
     this.modalRef.close(false);
   }
 
-  validateDomain(control: AbstractControl): Observable<ValidationErrors | null> {
+  validateDomain(
+    control: AbstractControl
+  ): Observable<ValidationErrors | null> {
     return timer(300).pipe(
       switchMap(() => {
-        return this.partnersApiService.validateDomain(`${control.value}${this.partnersApiService.endpointUrl}`).pipe(
-          map((res) => {
-            return res && control.value !== this.data.domain.replace(this.partnersApiService.endpointUrl, '') ? { exits: true } : null;
-          })
-        );
+        return this.partnersApiService
+          .validateDomain(
+            `${control.value}${this.partnersApiService.endpointUrl}`
+          )
+          .pipe(
+            map((res) => {
+              return res &&
+                control.value !==
+                  this.data.domain.replace(
+                    this.partnersApiService.endpointUrl,
+                    ''
+                  )
+                ? { exits: true }
+                : null;
+            })
+          );
       })
     );
   }
@@ -63,13 +92,36 @@ export class PartnersEditComponent implements OnInit {
       name: [this.data?.name, [TValidators.required]],
       representative: [this.data?.representative, [TValidators.required]],
       address: [this.data?.address, [TValidators.required]],
-      domain: [this.data?.domain.replace('.beautyup.asia', ''), [TValidators.required, TValidators.maxLength(10)], this.validateDomain.bind(this)],
-      phoneNumber: [this.data.phoneNumber, [TValidators.required, TValidators.phoneNumber]],
-      size: [this.data.size, [TValidators.required, TValidators.maxLength(6), TValidators.min(1), TValidators.onlyNumber()]],
+      domain: [
+        this.data?.domain.replace('.beautyup.asia', ''),
+        [TValidators.required, TValidators.maxLength(10)],
+        this.validateDomain.bind(this),
+      ],
+      phoneNumber: [
+        this.data.phoneNumber,
+        [TValidators.required, TValidators.phoneNumber],
+      ],
+      size: [
+        this.data.size,
+        [
+          TValidators.required,
+          TValidators.maxLength(6),
+          TValidators.min(1),
+          TValidators.onlyNumber(),
+        ],
+      ],
       email: [this.data.email, [TValidators.required, TValidators.emailRules]],
       settings: this.fb.group({
-        maxCourses: [Number(this.data?.settings?.maxCourses), [TValidators.required, TValidators.maxLength(3), TValidators.min(1), TValidators.onlyNumber()]],
-      })
+        maxFileSizeUpload: [
+          Number(this.data?.settings?.maxFileSizeUpload),
+          [
+            TValidators.required,
+            TValidators.maxLength(3),
+            TValidators.min(1),
+            TValidators.onlyNumber(),
+          ],
+        ],
+      }),
     });
   }
 }
--- src/app/master/partners/step-2/step-2.component.ts
+++ src/app/master/partners/step-2/step-2.component.ts
@@ -1,5 +1,10 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
 import { PartnersApiService } from '@shared/api/partners.api.service';
 import { Ultilities } from '@shared/extentions/Ultilities';
 import { TValidators } from '@shared/extentions/validators';
@@ -9,10 +14,9 @@ import { map, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-step-2',
   templateUrl: './step-2.component.html',
-  styleUrls: ['./step-2.component.scss']
+  styleUrls: ['./step-2.component.scss'],
 })
 export class Step2Component implements OnInit {
-
   @Input() form: FormGroup;
   @Input() currentStep: number;
   @Output() currentStepChange = new EventEmitter();
@@ -21,24 +25,41 @@ export class Step2Component implements OnInit {
   constructor(
     private fb: FormBuilder,
     private partnersApiService: PartnersApiService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
-      domain: [this.form.get('domain').value, [TValidators.required, TValidators.maxLength(10)], this.validateDomain.bind(this)],
-      maxCourses: [this.form.value.settings?.maxCourses, [TValidators.required, TValidators.maxLength(3), TValidators.min(1), TValidators.onlyNumber()]],
+      domain: [
+        this.form.get('domain').value,
+        [TValidators.required, TValidators.maxLength(10)],
+        this.validateDomain.bind(this),
+      ],
+      maxFileSizeUpload: [
+        this.form.value.settings?.maxFileSizeUpload,
+        [
+          TValidators.required,
+          TValidators.maxLength(3),
+          TValidators.min(1),
+          TValidators.onlyNumber(),
+        ],
+      ],
     });
   }
 
-  validateDomain(control: AbstractControl): Observable<ValidationErrors | null> {
+  validateDomain(
+    control: AbstractControl
+  ): Observable<ValidationErrors | null> {
     return timer(300).pipe(
       switchMap(() => {
-        return this.partnersApiService.validateDomain(`${control.value}${this.partnersApiService.endpointUrl}`).pipe(
-          map(res => {
-            return res ? { exits: true } : null;
-          })
-        );
+        return this.partnersApiService
+          .validateDomain(
+            `${control.value}${this.partnersApiService.endpointUrl}`
+          )
+          .pipe(
+            map((res) => {
+              return res ? { exits: true } : null;
+            })
+          );
       })
     );
   }
