d16bfdba6b6608be8b8cbcb886515146f1cadaec
Vu Le <vu.amsandcoffee@gmail.com>
2021-07-16 11:15:40 +0700

    status

--- src/app/user/student/student-detail/student-detail.component.ts
+++ src/app/user/student/student-detail/student-detail.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CourseApiService } from '@shared/api/course.api.service';
@@ -17,7 +18,7 @@ import { Meta, User } from 'types/typemodel';
 @Component({
   selector: 'app-student-detail',
   templateUrl: './student-detail.component.html',
-  styleUrls: ['./student-detail.component.scss']
+  styleUrls: ['./student-detail.component.scss'],
 })
 export class StudentDetailComponent implements OnInit {
   studentStatusOptions = StudentStatusOptions;
@@ -30,11 +31,15 @@ export class StudentDetailComponent implements OnInit {
   metaCertificate: Meta;
   coursePageIndex = 1;
   get btnUserStatus() {
-    return this.user && this.user.status === UserStatus.InActive ? 'Mở khóa tài khoản' : 'Khóa tài khoản';
+    return this.user && this.user.status === UserStatus.InActive
+      ? 'Mở khóa tài khoản'
+      : 'Khóa tài khoản';
   }
 
   get confirmMessage() {
-    return this.user && this.user.status === UserStatus.InActive ? 'Bạn có chắc chắn muốn mở khóa tài khoản học viên này không?' : 'Bạn có chắc chắn muốn khóa tài khoản học viên này không?';
+    return this.user && this.user.status === UserStatus.InActive
+      ? 'Bạn có chắc chắn muốn mở khóa tài khoản học viên này không?'
+      : 'Bạn có chắc chắn muốn khóa tài khoản học viên này không?';
   }
   registeredEventListData = [];
   certificateObtainedListData = [];
@@ -48,7 +53,7 @@ export class StudentDetailComponent implements OnInit {
     private courseApi: CourseApiService,
     private eventApi: EventApiService,
     private userCertificateApi: UserCertificationApiService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.userId = this.route.snapshot.paramMap.get('id');
@@ -56,61 +61,83 @@ export class StudentDetailComponent implements OnInit {
       user: this.getUser(),
       event: this.getEvents({ limit: 100 }),
       course: this.getCourse(),
-      certificate: this.getCertificates({ limit: 100 })
+      certificate: this.getCertificates({ limit: 100 }),
     }).subscribe();
   }
 
   inActiveAccount(id: string, status: UserStatus) {
     const message = status === UserStatus.Active ? 'Khóa' : 'Mở khóa';
     const next = () => {
-      this.user.status = status === UserStatus.Active ? UserStatus.InActive : UserStatus.Active;
-      this.notification.success('Thành công', `${message} tài khoản học viên thành công!`);
+      this.user.status =
+        status === UserStatus.Active ? UserStatus.InActive : UserStatus.Active;
+      this.notification.success(
+        'Thành công',
+        `${message} tài khoản học viên thành công!`
+      );
     };
-    const error = () => {
-      this.notification.success('Thất bại', `${message} tài khoản học viên thất bại!`);
+    const error = (error: HttpErrorResponse) => {
+      if (error.status === 412) {
+        this.notification.warning('Cảnh báo', error.error.message);
+      }
+      this.notification.error(
+        'Thất bại',
+        `${message} tài khoản học viên thất bại!`
+      );
     };
     const data = {
       id,
-      status: status === UserStatus.Active ? UserStatus.InActive : UserStatus.Active
+      status:
+        status === UserStatus.Active ? UserStatus.InActive : UserStatus.Active,
     };
     this.userApi.updateStatus(id, data).subscribe(next, error);
   }
 
   getCourse(params = {}) {
-    return this.courseApi.getByUser(this.userId, params).pipe(tap(res => {
-      this.metaCourse = res.meta;
-      this.itemData = res.items;
-    }));
+    return this.courseApi.getByUser(this.userId, params).pipe(
+      tap((res) => {
+        this.metaCourse = res.meta;
+        this.itemData = res.items;
+      })
+    );
   }
 
   getEvents(params = {}) {
-    return this.eventApi.getByUser(this.userId, params).pipe(map(x => {
-      x.items.forEach(y => {
-        if (moment().isBefore(y.startAt)) {
-          y.status = 'Wait';
-        } else {
-          y.status = moment().isBetween(y.startAt, y.endAt) ? 'Happening' : 'Done';
-        }
-        y.startAt = moment(y.startAt).format('HH:mm DD-MM-YYYY');
-      });
-      return x;
-    }), tap(res => {
-      this.metaEvent = res.meta;
-      this.registeredEventListData = res.items;
-    }));
+    return this.eventApi.getByUser(this.userId, params).pipe(
+      map((x) => {
+        x.items.forEach((y) => {
+          if (moment().isBefore(y.startAt)) {
+            y.status = 'Wait';
+          } else {
+            y.status = moment().isBetween(y.startAt, y.endAt)
+              ? 'Happening'
+              : 'Done';
+          }
+          y.startAt = moment(y.startAt).format('HH:mm DD-MM-YYYY');
+        });
+        return x;
+      }),
+      tap((res) => {
+        this.metaEvent = res.meta;
+        this.registeredEventListData = res.items;
+      })
+    );
   }
 
   getCertificates(params = {}) {
-    return this.userCertificateApi.getByUser(this.userId, params).pipe(tap(res => {
-      this.metaCertificate = res.meta;
-      this.certificateObtainedListData = res.items;
-    }));
+    return this.userCertificateApi.getByUser(this.userId, params).pipe(
+      tap((res) => {
+        this.metaCertificate = res.meta;
+        this.certificateObtainedListData = res.items;
+      })
+    );
   }
 
   getUser() {
-    return this.studentApi.getById(this.userId).pipe(tap(res => {
-      this.user = res;
-    }));
+    return this.studentApi.getById(this.userId).pipe(
+      tap((res) => {
+        this.user = res;
+      })
+    );
   }
 
   onParamsCourseChanged(event) {
