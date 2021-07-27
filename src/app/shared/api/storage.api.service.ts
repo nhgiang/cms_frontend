70aa679bd7da99a9e94f2a7298c675b6d4156f8f
Vu Le <vu.amsandcoffee@gmail.com>
2021-07-27 18:50:01 +0700

    add compression

--- src/app/shared/api/storage.api.service.ts
+++ src/app/shared/api/storage.api.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AnonymousCredential, BlobServiceClient } from '@azure/storage-blob';
-import { from, Observable, of } from 'rxjs';
-import { map, mapTo, switchMap, tap } from 'rxjs/operators';
+import { forkJoin, from, Observable, of } from 'rxjs';
+import { map, mapTo, mergeMap, switchMap, tap } from 'rxjs/operators';
 import { VideoAsset } from 'types/typemodel';
 import { BaseApi } from './base-api';
 import { v4 } from 'uuid';
+import { reduce } from 'lodash';
+declare var imageCompression: any;
+const options = {
+  maxSizeMB: 1,
+  useWebWorker: true,
+};
+// async function compress(image) {
+//   return await imageCompression(image, options);
+// }
 
 @Injectable({
   providedIn: 'root',
@@ -21,10 +30,20 @@ export class StorageApiService extends BaseApi {
       return of(file as string);
     }
     const form = new FormData();
-    form.append('file', file, fileName || (file as any).name || 'unknownfile');
-    return this.httpClient
-      .post<any>(this.createUrl('/upload'), form)
-      .pipe(map((result: { path: string }) => result.path));
+    return from(imageCompression(file as File, options)).pipe(
+      switchMap((compressed: any) => {
+        console.info('File gốc size: ', file.size);
+        console.info('File nén: ', compressed.size);
+        form.append(
+          'file',
+          compressed,
+          fileName || (file as any).name || 'unknownfile'
+        );
+        return this.httpClient
+          .post<any>(this.createUrl('/upload'), form)
+          .pipe(map((result: { path: string }) => result.path));
+      })
+    );
   }
 
   uploadFiles(files: Blob[] | File[] | string[] | any[]): Observable<string[]> {
@@ -33,18 +52,32 @@ export class StorageApiService extends BaseApi {
     }
     const form = new FormData();
     let fileNames = [];
-    files.forEach((file) => {
-      if (typeof file !== 'string') {
-        form.append('files', file, file.name || 'unknownfile');
-      } else {
-        fileNames.push(file);
-      }
-    });
-    return this.httpClient.post<any>(this.createUrl('/uploads'), form).pipe(
-      map((res: any[]) => {
-        fileNames = [...fileNames, ...res.map((file) => file.path)];
-        return fileNames;
+    return forkJoin(
+      (files as any[]).map((file: any) => {
+        if (typeof file !== 'string')
+          return imageCompression(file as File, options);
+        else return of(file as string);
       })
+    ).pipe(
+      tap((files: any[]) => {
+        files.forEach((file: any) => {
+          if (typeof file !== 'string')
+            form.append(
+              'files',
+              file as File,
+              (file.name as any) || 'unknownfile'
+            );
+          else fileNames.push(file);
+        });
+      }),
+      switchMap(() =>
+        this.httpClient.post<any>(this.createUrl('/uploads'), form).pipe(
+          map((res: any[]) => {
+            fileNames = [...fileNames, res.map((file) => file.path)];
+            return fileNames;
+          })
+        )
+      )
     );
   }
 
@@ -54,8 +87,10 @@ export class StorageApiService extends BaseApi {
     }
     const form = new FormData();
     form.append('file', file, fileName || (file as any).name || 'unknownfile');
-    return this.httpClient
-      .post<VideoAsset>(this.createUrl('/upload-video'), form)
+    return this.httpClient.post<VideoAsset>(
+      this.createUrl('/upload-video'),
+      form
+    );
   }
 
   createUploadUrl(body: { name: string; size: number }) {
@@ -69,27 +104,27 @@ export class StorageApiService extends BaseApi {
     this.file = file;
     const fileName = `azure-video-${v4()}`;
     return this.createUploadUrl({ name: fileName, size: file.size }).pipe(
-      switchMap(res => {
-        return this.putToAzure(res.uploadUrl, file, res.fileName).pipe(mapTo(res.fileName));
+      switchMap((res) => {
+        return this.putToAzure(res.uploadUrl, file, res.fileName).pipe(
+          mapTo(res.fileName)
+        );
       })
     );
   }
 
-  putToAzure(
-    uploadUrl: string,
-    file: File,
-    fileName: string
-  ): Observable<any> {
+  putToAzure(uploadUrl: string, file: File, fileName: string): Observable<any> {
     const anonymousCredential = new AnonymousCredential();
     const blobClient = new BlobServiceClient(uploadUrl, anonymousCredential);
     const containerClient = blobClient.getContainerClient('');
     const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-    return from(blockBlobClient.uploadData(file, {
-      blockSize: 8 * 1024 * 1024, // 8MB Block size
-      blobHTTPHeaders: {
-        blobContentType: file.type
-      }
-    }));
+    return from(
+      blockBlobClient.uploadData(file, {
+        blockSize: 8 * 1024 * 1024, // 8MB Block size
+        blobHTTPHeaders: {
+          blobContentType: file.type,
+        },
+      })
+    );
   }
 
   uploadVideoPublic(file: File | string) {
@@ -99,9 +134,11 @@ export class StorageApiService extends BaseApi {
     this.file = file;
     const fileName = `azure-video-${v4()}`;
     return this.createUploadUrl({ name: fileName, size: file.size }).pipe(
-      switchMap(res => {
-        return this.putToAzure(res.uploadUrl, file, res.fileName).pipe(mapTo(res.fileName));
-      }),
+      switchMap((res) => {
+        return this.putToAzure(res.uploadUrl, file, res.fileName).pipe(
+          mapTo(res.fileName)
+        );
+      })
     );
   }
 
