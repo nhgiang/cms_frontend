71003e9cc892fa7c95e61594961b0d308cf0dacc
vietlh <viet150597@gmail.com>
2021-08-03 23:17:27 +0700

    fix

--- src/app/shared/components/ck-editor/ck-editor.component.ts
+++ src/app/shared/components/ck-editor/ck-editor.component.ts
@@ -64,7 +64,7 @@ export class CkEditorComponent implements ControlValueAccessor {
         },
         {
           name: 'resizeImage:100',
-          value: null,
+          value: '100',
           label: '100%'
         },
       ],
