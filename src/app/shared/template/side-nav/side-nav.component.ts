42600c74bfae74873ad36b3b57fe4e62fea35c49
Vu Le <vu.amsandcoffee@gmail.com>
2021-08-20 09:16:59 +0700

    fix-bug

--- src/app/shared/template/side-nav/side-nav.component.ts
+++ src/app/shared/template/side-nav/side-nav.component.ts
@@ -4,9 +4,10 @@ import { ThemeConstantService } from '@shared/services/theme-constant.service';
 import { User } from 'types/typemodel';
 import { ROUTES } from './side-nav-routes.config';
 
+const ROOT_PARTNER_ID = '00000000-0000-0000-0000-000000000000';
 @Component({
   selector: 'app-sidenav',
-  templateUrl: './side-nav.component.html'
+  templateUrl: './side-nav.component.html',
 })
 export class SideNavComponent implements OnInit {
   menuItems: any[];
@@ -14,16 +15,25 @@ export class SideNavComponent implements OnInit {
   isSideNavDark: boolean;
   isExpand: boolean;
   currentUser: User;
+  isRoot: boolean;
   constructor(
     private themeService: ThemeConstantService,
     private authService: AuthenticationService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe(user => this.currentUser = user); this.menuItems = ROUTES.filter(menuItem => menuItem);
-    this.themeService.isMenuFoldedChanges.subscribe(isFolded => this.isFolded = isFolded);
-    this.themeService.isExpandChanges.subscribe(isExpand => this.isExpand = isExpand);
-    this.themeService.isSideNavDarkChanges.subscribe(isDark => this.isSideNavDark = isDark);
+    this.authService.currentUser.subscribe((user) => (this.currentUser = user));
+    this.menuItems = ROUTES.filter((menuItem) => menuItem);
+    this.themeService.isMenuFoldedChanges.subscribe(
+      (isFolded) => (this.isFolded = isFolded)
+    );
+    this.themeService.isExpandChanges.subscribe(
+      (isExpand) => (this.isExpand = isExpand)
+    );
+    this.themeService.isSideNavDarkChanges.subscribe(
+      (isDark) => (this.isSideNavDark = isDark)
+    );
+    this.isRoot = this.authService.partnerId === ROOT_PARTNER_ID;
   }
 
   closeMobileMenu(): void {
