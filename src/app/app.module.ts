1150e0bdeb00367b1a99edf5b0882a6c87c78e0d
Vu Le <vu.amsandcoffee@gmail.com>
2021-08-20 10:38:01 +0700

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,7 @@ const appInit = (
     return partnersApi
       .getDomain(
         domain.replace('cms.', '').includes('localhost')
-          ? 'qa.beautyup.asia'
+          ? 'abc.qa.beautyup.asia'
           : domain.replace('cms.', '')
       )
       .toPromise()
